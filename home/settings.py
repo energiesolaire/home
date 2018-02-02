57ba056a6f09ae7ebfb7209d7c6c1e9775cf744c
energiesolaire <36019884+energiesolaire@users.noreply.github.com>
2018-02-02 15:00:18 -0200

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'home.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': 'energiesolaire.mysql.pythonanywhere-services.com',
     }
 }
-'''
+
 
 
 # Password validation
